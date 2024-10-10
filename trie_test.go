196b43fa4ff2d877dc506ce30bd60898b6e6020f
Ethan Higgins <ehiggins0@protonmail.com>
2024-10-10 15:44:26 -0600

    Modified visualization tools and tests

--- trie_test.go
+++ trie_test.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 	"testing"
 	"time"
+	"unsafe"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -20,21 +21,28 @@ func TestTrieFromPatterns(t *testing.T) {
 	// for i, pattern := range patternsJSON {
 	// 	patterns[X(fmt.Sprintf("pattern_%d", i))] = pattern
 	// }
-	// patterns[X("pattern_2")] = `{"field_0":["foo", "bar"], "field_1":["asdf", "qwer"]}`
-	// patterns[X("pattern_3")] = `{"field_0":["foo", "baz"], "field_1":["asdf", "zxcv"]}`
-	// patterns[X("pattern_0")] = `{"field_0":[{"prefix": "foo"}], "field_1":[{"prefix": "bar"}]}`
-	patterns[X("pattern_0")] = `{"field_0":[{"equals-ignore-case": "fOo"}]}`
+	patterns[X("pattern_0")] = `{"field_0":["foo", "bar"], "field_1":["asdf", "qwer"]}`
+	patterns[X("pattern_1")] = `{"field_0":["foo", "baz"], "field_1":["asdf", "zxcv"]}`
+	patterns[X("pattern_2")] = `{"field_0":[{"prefix": "foo"}], "field_1":[{"prefix": "bar"}]}`
+	patterns[X("pattern_3")] = `{"field_0":[{"equals-ignore-case": "fOo"}]}`
+	patterns[X("pattern_4")] = `{"field_0":["aaaa", "abaa"], "field_1":["cccc", "cbaa"]}`
+	patterns[X("pattern_5")] = `{"field_1":["bbbb", "bbaa"]}`
+	patterns[X("pattern_6")] = `{"field_1":[{"prefix": "bbaa"}]}`
+	patterns[X("pattern_7")] = `{"field_1":["bbbbb", "bbaa"]}`
 
 	fmt.Printf("Patterns: %v\n", patterns)
 
 	start := time.Now()
 	fields := make(map[string]struct{})
-	trie, err := trieFromPatterns(patterns, &fields)
+	tries, err := trieFromPatterns(patterns, &fields)
 	if err != nil {
 		t.Fatalf("Error building trie: %v", err)
 	}
 	t.Logf("Time to build trie: %v", time.Since(start))
-	t.Logf("Trie:\n%v", visualizePathTrie(trie))
+
+	t.Logf("Trie:\n%v", visualizePathTrie(tries["field_0"]))
+	t.Logf("Trie:\n%v", visualizePathTrie(tries["field_1"]))
+	t.Logf("Mermaid Diagram for all tries:\n%v", visualizeTriesAsMermaid(tries))
 }
 
 func TestMatcherFromPatterns(t *testing.T) {
@@ -64,24 +72,25 @@ func TestMatcherFromPatterns(t *testing.T) {
 		{
 			name: "Different types of patterns",
 			patterns: map[X]string{
-				// X("pattern_0"): `{"field_0":[{"prefix": "foo"}], "field_1":["foo", {"prefix": "bar"}, "baz"]}`,
-				X("pattern_1"): `{"field_0":[{"equals-ignore-case": "fOo"}], "field_1":["foo", {"prefix": "bar"}, "baz"]}`,
-				// X("pattern_1"): `{"field_0":[{"anything-but": ["a", "b"]}]}`,
-				// X("pattern_1"): `{"field_0":["foo", "bar"]}`,
-				// X("pattern_1"): `{"field_0":[{"anything-but": ["baz", "qux"]}], "field_1":[{"wildcard": "a*f"}]}`,
-				// X("pattern_2"): `{"field_0":[{"equals-ignore-case": "Hello"}], "field_1":[{"wildcard": "*.jpg"}]}`,
+				// X("pattern_0"): `{"field_0":[{"prefix": "bar"}], "field_1":["foo", {"prefix": "bar"}, "baz", {"equals-ignore-case": "pReSeNt"}]}`,
+				// X("pattern_1"): `{"field_0":[{"equals-ignore-case": "fOo"}], "field_1":["foo", {"prefix": "bar"}, "baz"]}`,
+				// X("pattern_2"): `{"field_0":[{"anything-but": ["a", "b"]}]}`,
+				X("pattern_3"): `{"field_0":["foo", "bar"]}`,
+				X("pattern_4"): `{"field_1":["foo", "bar"]}`,
+				// X("pattern_4"): `{"field_0":[{"anything-but": ["baz", "qux"]}], "field_1":[{"wildcard": "a*f"}]}`,
+				// X("pattern_5"): `{"field_0":[{"equals-ignore-case": "Hello"}], "field_1":[{"wildcard": "*.jpg"}]}`,
 			},
 			events: [][]byte{
 				[]byte(`{"field_0": "foo", "field_1": "asdf"}`),
-				// []byte(`{"field_0": "barcode", "field_1": "present"}`),
-				// []byte(`{"field_0": "hello", "field_1": "image.jpg"}`),
-				// []byte(`{"field_0": "baz", "field_1": "abcf"}`),
+				[]byte(`{"field_0": "barcode", "field_1": "bar"}`),
+				[]byte(`{"field_0": "hello", "field_1": "image.jpg"}`),
+				[]byte(`{"field_0": "baz", "field_1": "abcf"}`),
 			},
 			expected: [][]X{
-				{X("pattern_0"), X("pattern_1")},
-				// {X("pattern_0")},
-				// {X("pattern_2")},
-				// {X("pattern_1")},
+				{X("pattern_3")},
+				{X("pattern_4")},
+				{},
+				{},
 			},
 		},
 	}
@@ -96,21 +105,18 @@ func TestMatcherFromPatterns(t *testing.T) {
 				oldMatcher.addPattern(x, pattern)
 			}
 
-			t.Logf("Matcher: %v", unravelMatcher(matcher, false))
-			t.Logf("Old Matcher: %v", unravelMatcher(oldMatcher, false))
-
-			// for i, event := range tc.events {
-			// 	matches, err := matcher.matchesForJSONEvent(event)
-			// 	assert.NoError(t, err)
-			// 	oldMatches, err := oldMatcher.matchesForJSONEvent(event)
-			// 	assert.NoError(t, err)
-			// 	assert.Equal(t, tc.expected[i], matches)
-			// 	if !assert.Equal(t, oldMatches, matches) {
-			// 		t.Logf("Patterns: %v, Event: %v, Matches: %v", tc.patterns, string(event), matches)
-			// 		t.Logf("Matcher: %v", unravelMatcher(matcher))
-			// 		t.Logf("Old Matcher: %v", unravelMatcher(oldMatcher))
-			// 	}
-			// }
+			for i, event := range tc.events {
+				matches, err := matcher.matchesForJSONEvent(event)
+				assert.NoError(t, err)
+				oldMatches, err := oldMatcher.matchesForJSONEvent(event)
+				assert.NoError(t, err)
+				assert.Equal(t, tc.expected[i], matches)
+				if !assert.Equal(t, oldMatches, matches) {
+					t.Logf("Patterns: %v, Event: %v, Matches: %v", tc.patterns, string(event), matches)
+					t.Logf("Matcher: %v", unravelMatcher(matcher, true))
+					t.Logf("Old Matcher: %v", unravelMatcher(oldMatcher, true))
+				}
+			}
 		})
 	}
 }
@@ -219,7 +225,7 @@ func TestMatcherFromSimplePatterns(t *testing.T) {
 // }
 
 func BenchmarkMatcherFromPatterns(b *testing.B) {
-	patternsJSON := generatePatterns(10000, []int{1000})
+	patternsJSON := generatePatterns(100, []int{1000, 100})
 	patterns := make(map[X]string)
 
 	for i, pattern := range patternsJSON {
@@ -291,5 +297,137 @@ func visualizePathTrieRecursive(buf *bytes.Buffer, trie *pathTrie, depth int) {
 	for path, nextTrie := range trie.node.transition {
 		buf.WriteString(fmt.Sprintf("%sTransition to field: %s\n", indent, path))
 		visualizePathTrieRecursive(buf, nextTrie, depth+1)
+	}
+}
+
+func visualizeTriesAsMermaid(tries map[string]*pathTrie) string {
+	var buf bytes.Buffer
+	buf.WriteString("graph TD\n")
+	visitedNodes := make(map[uintptr]string)
+	visitedLinks := make(map[string]bool)
+
+	// Create a root node to connect all tries
+	rootId := "root"
+	buf.WriteString(fmt.Sprintf("    %s[Root]\n", rootId))
+
+	for field, trie := range tries {
+		trieId := fmt.Sprintf("%p", trie)
+		linkKey := fmt.Sprintf("%s-->%s", rootId, trieId)
+		if !visitedLinks[linkKey] {
+			buf.WriteString(fmt.Sprintf("    %s --> %s[%s]\n", rootId, trieId, field))
+			visitedLinks[linkKey] = true
+		}
+		visualizePathTrieAsMermaidRecursive(&buf, trie, trieId, 0, visitedNodes, visitedLinks)
+	}
+
+	return buf.String()
+}
+
+func visualizePathTrieAsMermaid(buf *bytes.Buffer, trie *pathTrie, parentId string) string {
+	var localBuf bytes.Buffer
+	localBuf.WriteString("graph TD\n")
+	visitedNodes := make(map[uintptr]string)
+	visitedLinks := make(map[string]bool)
+	visualizePathTrieAsMermaidRecursive(&localBuf, trie, parentId, 0, visitedNodes, visitedLinks)
+	return localBuf.String()
+}
+
+func visualizePathTrieAsMermaidRecursive(buf *bytes.Buffer, trie *pathTrie, parentId string, depth int, visitedNodes map[uintptr]string, visitedLinks map[string]bool) {
+	currentPtr := uintptr(unsafe.Pointer(trie))
+	currentId, exists := visitedNodes[currentPtr]
+	if !exists {
+		currentId = fmt.Sprintf("%p", trie)
+		visitedNodes[currentPtr] = currentId
+		buf.WriteString(fmt.Sprintf("    %s[%s<br />%p]\n", currentId, trie.path, trie))
+	}
+
+	visualizeTrieNodeAsMermaid(buf, trie.node, &currentId, depth+1, visitedNodes, visitedLinks)
+
+	for path, nextTrie := range trie.node.transition {
+		nextPtr := uintptr(unsafe.Pointer(nextTrie))
+		nextId, exists := visitedNodes[nextPtr]
+		if !exists {
+			nextId = fmt.Sprintf("%p", nextTrie)
+			visitedNodes[nextPtr] = nextId
+			buf.WriteString(fmt.Sprintf("    %s[%s<br />%p]\n", nextId, path, nextTrie))
+		}
+
+		linkKey := fmt.Sprintf("%s-->%s", currentId, nextId)
+		if !visitedLinks[linkKey] {
+			buf.WriteString(fmt.Sprintf("    %s --> %s\n", currentId, nextId))
+			visitedLinks[linkKey] = true
+		}
+
+		visualizePathTrieAsMermaidRecursive(buf, nextTrie, currentId, depth+1, visitedNodes, visitedLinks)
+	}
+}
+
+func visualizeTrieNodeAsMermaid(buf *bytes.Buffer, node *trieNode, parentId *string, depth int, visitedNodes map[uintptr]string, visitedLinks map[string]bool) {
+	currentPtr := uintptr(unsafe.Pointer(node))
+	currentId, exists := visitedNodes[currentPtr]
+	if !exists {
+		currentId = fmt.Sprintf("%p", node)
+		visitedNodes[currentPtr] = currentId
+	}
+
+	// Handle early end state (isEnd) and wildcard (isWildcard)
+	if node.isEnd || node.isWildcard {
+		endId := fmt.Sprintf("%s_end", currentId)
+		endLabel := "END"
+		if node.isWildcard {
+			endLabel = "WILDCARD"
+		}
+		patterns := make([]string, 0, len(node.memberOfPatterns))
+		for pattern := range node.memberOfPatterns {
+			patterns = append(patterns, pattern.(string))
+		}
+		patternsStr := strings.Join(patterns, ", ")
+		buf.WriteString(fmt.Sprintf("    %s((%s<br />%p<br />%s))\n", endId, endLabel, node, patternsStr))
+
+		endLinkKey := fmt.Sprintf("%s-->%s", *parentId, endId)
+		if !visitedLinks[endLinkKey] {
+			buf.WriteString(fmt.Sprintf("    %s --> %s\n", *parentId, endId))
+			visitedLinks[endLinkKey] = true
+		}
+	}
+
+	for ch, child := range node.children {
+		childPtr := uintptr(unsafe.Pointer(child))
+		childId, exists := visitedNodes[childPtr]
+		if !exists {
+			childId = fmt.Sprintf("%p", child)
+			visitedNodes[childPtr] = childId
+			if ch == '"' {
+				buf.WriteString(fmt.Sprintf("    %s[QUOTE<br />%p]\n", childId, child))
+			} else {
+				buf.WriteString(fmt.Sprintf("    %s[%c<br />%p]\n", childId, ch, child))
+			}
+		}
+
+		linkKey := fmt.Sprintf("%s-->%s", *parentId, childId)
+		if !visitedLinks[linkKey] {
+			buf.WriteString(fmt.Sprintf("    %s --> %s\n", *parentId, childId))
+			visitedLinks[linkKey] = true
+		}
+
+		visualizeTrieNodeAsMermaid(buf, child, &childId, depth+1, visitedNodes, visitedLinks)
+	}
+
+	for path, nextTrie := range node.transition {
+		nextPtr := uintptr(unsafe.Pointer(nextTrie))
+		nextId, exists := visitedNodes[nextPtr]
+		if !exists {
+			nextId = fmt.Sprintf("%p", nextTrie)
+			visitedNodes[nextPtr] = nextId
+			buf.WriteString(fmt.Sprintf("    %s[%s<br />%p]\n", nextId, path, nextTrie))
+		}
+
+		linkKey := fmt.Sprintf("%s-->%s", *parentId, nextId)
+		if !visitedLinks[linkKey] {
+			buf.WriteString(fmt.Sprintf("    %s --> %s\n", *parentId, nextId))
+			visitedLinks[linkKey] = true
+		}
+
+		visualizePathTrieAsMermaidRecursive(buf, nextTrie, *parentId, depth+1, visitedNodes, visitedLinks)
 	}
 }
